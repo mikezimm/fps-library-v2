068d7076091527840e6b140b711d901ad4f1ac94
mikezimm <49648086+mikezimm@users.noreply.github.com>
2022-12-10 04:36:33 -0500

    Add single build error comments in index file

--- src/pnpjs/Users/indexes/GetUserPermissions.ts
+++ src/pnpjs/Users/indexes/GetUserPermissions.ts
@@ -1,6 +1,17 @@
 
+/**
+ *  Getting this warning when trying to export IUserPermissionsInfo
+ * 
+  WARNING in ./src/pnpjs/Users/indexes/GetUserPermissions.ts 2:0-32
+    export 'IUserPermissionsInfo' (reexported as 'IUserPermissionsInfo') was not found in '../interfaces/IUserPermissionsInfo' (module has no exports)
+    @ ./src/pnpjs/Users/indexes/index.ts 7:0-37 7:0-37
+    @ ./src/pnpjs/index.ts 6:0-38 6:0-38
+    @ ./src/index.ts 5:0-30 5:0-30
+ */
+
 import type { getUserPermissions } from "../calls/getUserPermissions";
 import { IUserPermissionsInfo } from "../interfaces/IUserPermissionsInfo";
 
 export { getUserPermissions, IUserPermissionsInfo };
+// export { getUserPermissions, };
 
