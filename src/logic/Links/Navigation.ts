8f649f55ab6394c04b87a1704a1d290a4b5b1c94
mikezimm <49648086+mikezimm@users.noreply.github.com>
2022-12-12 00:36:53 -0500

    Add SiteNavigation, update Changelog

---
+++ src/logic/Links/Navigation.ts
@@ -0,0 +1,23 @@
+
+import { PageContext } from '@microsoft/sp-page-context';
+
+
+export function goToParentSite( e: any, pageContext: PageContext ): void {
+  // let e: any = event;
+  console.log('goToParentSite event:',e);
+
+  let target = e.ctrlKey === true || e.altKey === true ? '_blank' : '_self';
+  let parentUrl = pageContext.web.absoluteUrl.substr(0, pageContext.web.absoluteUrl.lastIndexOf('/') );
+  window.open( parentUrl, target );
+
+}
+
+export function  goToHomePage( e: any, pageContext: PageContext ): void {
+  // let e: any = event;
+  console.log('goToHomePage event',e);
+
+  let target = e.ctrlKey === true || e.altKey === true ? '_blank' : '_self';
+  let parentUrl = pageContext.web.absoluteUrl;
+  window.open( parentUrl, target );
+
+}
